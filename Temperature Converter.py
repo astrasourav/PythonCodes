19de78a88513cc4a474dc8017803e90418278c56
astrasourav <50782204+astrasourav@users.noreply.github.com>
2019-05-22 22:26:35 +0530

    used (user defined functions) this code

--- Temperature Converter.py
+++ Temperature Converter.py
@@ -1,17 +1,20 @@
 # In this project we will convert temperature to (Degrees) or (Celcius),and the input will be given by the user
 
- print("""This is Temperature Converter program.
- Hit enter to continue""")
- x=str.lower(input("To convert the Temperature to Fahrenheit type (F), for Celsius type (C)"))
- y=int(input("Enter the temperature here :-"))
- if x=="c":
-    answer1=int((y-32)*(5/9))
-    print(answer1)
- elif x=="f":
-    answer2=int((y*1.8)+32)
-    print(answer2)
- else:
-    print("There is no input")
+def my_fun(x, y):
+    if x == "c":
+        ans = (y-32) * 0.55
+        return(ans)
+    elif x == "f":
+        ans = (y * 1.8) + 32
+        return(ans)
+    else:
+        print("Incorrent parameters")
+
+x = str.lower(input("f to convert to fahrenheit, c to convert to celcius"))
+y = float(input("enter temp"))
+
+ans = my_fun(x,y)
+print(ans)
     
     
     
