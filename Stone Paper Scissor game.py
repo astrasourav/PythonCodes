f7256cccb07a061960ab2e2b43e83217baa50ecf
astrasourav <50782204+astrasourav@users.noreply.github.com>
2019-05-24 13:37:37 +0530

    Final game code

---
+++ Stone Paper Scissor game.py
@@ -0,0 +1,34 @@
+# This is a game of Rock, Paper and Scissors
+
+
+# import random
+# print("This is stone, paper, scissor game")
+# x=str.lower(input("For Rock press(r), for Scissor press(s) and for Paper press(p)"))
+# list=["rock", "paper", "scissor"]
+# pc=random.choice(list)
+# print(x ,'vs', pc)
+# if x == "r":
+#    if pc == "rock":
+#        print("Tie")
+#    elif pc == "paper":
+#        print("Computer wins")
+#    else:
+#        print("User wins")
+# if x == "p":
+#    if pc == "paper":
+#        print("Tie")
+#    elif pc == "scissor":
+#        print("Computer wins")
+#    else:
+#        print("User wins")
+# if x == "s":
+#    if pc == "scissor":
+#        print("Tie")
+#    elif pc == "rock":
+#        print("Computer wins")
+#    else:
+#        print("User wins")
+
+
+#Just uncomment the above codes to use them.
+#Thank You :)
