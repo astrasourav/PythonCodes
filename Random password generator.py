b11740fd600a5c84940203d4b1f1d75e1739fcef
astrasourav <50782204+astrasourav@users.noreply.github.com>
2019-05-24 13:39:28 +0530

    Final game code

---
+++ Random password generator.py
@@ -0,0 +1,18 @@
+# This a program which randomly creates passwords and the length is set by the user
+
+#import random
+
+#length=int(input("Enter the length of your password you want to create :- "))
+#length2=int(input("How many times you want to create password :- "))
+#char='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^*()'
+#
+#for p in range(length2):
+#   answer = ''
+ #   for i in range(length):
+ #       answer += random.choice(char)
+#
+#    print(answer)
+
+
+#Just umcomment the above code to use them.
+#Thank You :)
