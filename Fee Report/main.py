95c282282dc051318aabc04682fe03d845b394c3
Sourav Kumar <50782204+astrasourav@users.noreply.github.com>
2020-10-10 11:12:13 +0530

    Update main.py

--- Fee Report/main.py
+++ Fee Report/main.py
@@ -1,5 +1,9 @@
-from FeeReport import FeeReport
-
+import tkinter as tk
+from fee_report import Fee_Report
 
 if __name__ == "__main__":
-    obj = FeeReport().fee_report()
+    root = tk.Tk()
+
+    application = Fee_Report(root)
+
+    root.mainloop()
